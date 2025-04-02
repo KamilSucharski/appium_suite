e1109433c69e8f4443463c53274d0b8f749d00f1
Kamil Sucharski <kamil.sucharski@apzumi.com>
2025-04-02 14:26:50 +0200

    Android/iOS standardization WIP

--- src/test/java/android/test/helper/SetPlatformAndroidHelper.kt
+++ src/test/java/android/test/helper/SetPlatformAndroidHelper.kt
@@ -3,6 +3,7 @@ package android.test.helper
 import common.util.junit.Helper
 import common.util.Platform
 import common.util.platform
+import common.util.platformSpecificInstructions
 import org.junit.Test
 import org.junit.experimental.categories.Category
 
@@ -12,6 +13,7 @@ class SetPlatformAndroidHelper {
     @Category(Helper::class)
     fun setPlatformAndroid() {
         platform = Platform.Android
+        platformSpecificInstructions.vibrateDevice(1)
     }
 
 }
